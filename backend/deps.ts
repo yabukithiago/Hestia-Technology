c62f4b5c1801481fc28850aecebbe7aff24d6d4f
danielalexis <danielalexis.o.pereira@gmail.com>
2024-03-13 19:42:10 +0000

    Update dependencies for Oak and Postgres

--- backend/deps.ts
+++ backend/deps.ts
@@ -3,6 +3,8 @@ export {
   Context,
   isHttpError,
   Router,
-} from "https://deno.land/x/oak@v12.5.0/mod.ts";
+} from "https://deno.land/x/oak@14.2.0/mod.ts";
 
-export type { Next } from "https://deno.land/x/oak@v12.5.0/mod.ts";
+export type { Next } from "https://deno.land/x/oak@14.2.0/mod.ts";
+
+export { Client } from "https://deno.land/x/postgres@0.19.3/mod.ts";
