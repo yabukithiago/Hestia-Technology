08b25aa4c829df466b597fac85def40df395f9d4
danielalexis <danielalexis.o.pereira@gmail.com>
2024-03-26 17:28:52 +0000

    Refactor search route and add database query***

--- backend/routes/search.ts
+++ backend/routes/search.ts
@@ -1,15 +1,25 @@
 import { Context } from "../deps.ts";
-//import { Client } from "../deps.ts";
+import { DBPool } from "../utils/db.ts";
 
 export class Search {
-  public static get(ctx: Context) {
+  public static async get(ctx: Context) {
     // Get the body of the request
     const input = ctx.request.url.searchParams.get("input");
     if (!input) {
       ctx.throw(400, "Param input is required");
     }
 
+    const db = await DBPool.connect();
+    const { rows: projectRows } = await db.queryObject<
+      { name: string; eid: string }
+    >(
+      "SELECT name, eid FROM gesconsultoria.project WHERE input LIKE %$1%",
+      [
+        `%${input}%`,
+      ],
+    );
+
     ctx.response.type = "json";
-    ctx.response.body = { "hello": "world" };
+    ctx.response.body = { projects: projectRows };
   }
 }
